a34effb656b2d55d381da8f090b3bc0f35dd3b6e
accelq <info@accelq.com>
2021-02-18 10:44:29 +0530

    Fixed build errors

--- deploy/run.bat
+++ deploy/run.bat
@@ -5,8 +5,11 @@ For /F "eol=# tokens=1,* delims==" %%A IN (data.properties) DO (
 	set %%A=%%B
 )
 
+set temp=%project_location%\src\lib\core-1.0-SNAPSHOT.jar
+
 echo Starting Jenkins loaded with accelQ Plugin on Port 8954
 cd /d "%project_location%"
+call mvn install:install-file -Dfile="%temp%" -DgroupId=core -DartifactId=aqCore -Dversion=1.0 -Dpackaging=jar
 call mvn hpi:run -Djetty.port=%jenkins_server_port%
 
 endlocal
