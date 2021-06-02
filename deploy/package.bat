51930713beceebc1b41e8b5cf3d1c3564c019c7c
accelq <info@accelq.com>
2021-06-02 10:40:09 +0530

    fixed core

--- deploy/package.bat
+++ deploy/package.bat
@@ -10,8 +10,3 @@ set temp=%project_location%\src\lib\core-1.0-SNAPSHOT.jar
 echo Packaging repo
 cd /d "%project_location%"
 call mvn install:install-file -Dfile="%temp%" -DgroupId=core -DartifactId=aqCore -Dversion=1.0 -Dpackaging=jar
-call mvn package
-
-endlocal
-pause > nul
-exit
