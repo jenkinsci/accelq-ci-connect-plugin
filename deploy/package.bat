61537722c1c45f167cd56aed193c1c3a71e69562
accelq <info@accelq.com>
2023-02-21 11:44:24 +0530

    added rest of files

--- deploy/package.bat
+++ deploy/package.bat
@@ -5,7 +5,7 @@ For /F "eol=# tokens=1,* delims==" %%A IN (data.properties) DO (
 	set %%A=%%B
 )
 
-set temp=%project_location%\src\lib\core-1.0-SNAPSHOT.jar
+set temp=%project_location%\core\target\core-1.0-SNAPSHOT.jar
 
 echo Packaging repo
 cd /d "%project_location%"
